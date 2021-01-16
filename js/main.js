ee788b35b28abe763dc454d32b689a0e8377df24
TheYushinator <ayushtvora@gmail.com>
2021-01-16 05:06:09 -0500

    a lot.

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@ openHome = () => {window.open("index.html","_self")}
 openTimer = () => {window.open("timer.html","_self")}
 openRecording = () => {window.open("recording.html","_self")}
 openStatistics = () => {window.open("analytics.html","_self")}
+
+let fs = require("fs");
+let textByLine;
+fs.readFile("doc/topics.txt", function(text){
+  textByLine = text.split("\n")
+  console.log(textByLine)
+});
