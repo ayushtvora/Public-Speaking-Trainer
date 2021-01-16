24c8a5f26a23e5d727b66b86d3c313eb995ebdc4
TheYushinator <ayushtvora@gmail.com>
2021-01-16 02:56:13 -0500

    added content to home page and added navigation buttons

---
+++ js/main.js
@@ -0,0 +1,4 @@
+openHome = () => {window.open("index.html","_self")}
+openTimer = () => {window.open("timer.html","_self")}
+openRecording = () => {window.open("recording.html","_self")}
+openStatistics = () => {window.open("analytics.html","_self")}
